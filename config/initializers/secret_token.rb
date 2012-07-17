7d12ad409664e31380d1d93dcc48e0b4376d9b70
Bhushan <bhushan@weboniselab.com>
2012-07-17 16:15:08 +0530

    Project File structure added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagicBox::Application.config.secret_token = '001564559f929d346616518fba29e5e91d128a00947a58874435d8e82732e077fd944cc0509483f0caeacd7404017b5a232d0ca807590e4fc81f4f256217263a'
